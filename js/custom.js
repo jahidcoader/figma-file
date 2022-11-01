945cdc768db1c2443f9d8d4291cff25ccd051aa2
jahidcoader <jahidhassan0430202@gmail.com>
2022-11-02 01:24:33 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$('.claint_main').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots:false,
+  arrows:true,
+  nextArrow: '.cliant_next',
+  prevArrow: '.cliant_prev',
+});
+	
